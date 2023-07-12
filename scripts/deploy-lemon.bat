79f6520168f1d3f4101813936aac831fe4552c69
Lucas Liu <sqooiddev@gmail.com>
2023-07-12 22:48:44 +1000

    Add plausible anon analytics

--- scripts/deploy-lemon.bat
+++ scripts/deploy-lemon.bat
@@ -1,5 +1,4 @@
-@REM pnpm run build
-rm build.tar
+pnpm run build
 tar -cf build.tar -C build .
 scp build.tar lemon:~/helium
 ssh lemon tar -xf ~/helium/build.tar -C ~/helium --overwrite
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
 declare const __THEME__: string;
 declare const __DOMAIN__: string;
 declare module 'remark-container';
+declare const plausible: (event: string, options: { props: { [prop: string]: string } }) => void;
